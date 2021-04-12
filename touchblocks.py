69adcfbe6010fbf7a5ea1473ba14687ec0704208
elggem <trilly@gmail.com>
2021-04-12 16:43:28 +0200

    grid alignment finally working

--- touchblocks.py
+++ touchblocks.py
@@ -21,45 +21,6 @@ from kivy.core.window import Window
 from random import random
 from math import sqrt
 
-
-
-
-
-
-
-
-
-
-
-
-class Grid():
-    def __init__(self, canvas, gridDim=[6,6], gridPos=[0,0], gridSize=[100,100]):
-        self.gridDim = gridDim
-        self.gridPos = gridPos
-        self.gridSize = gridSize
-
-        self.canvas = canvas
-
-    def draw(self):
-        with self.canvas:
-            Color(0,0,0)
-
-            for i in range(self.gridDim[0]):
-                for j in range(self.gridDim[1]):
-                    distanceX = self.gridSize[0] / self.gridDim[0]
-                    distanceY = self.gridSize[1] / self.gridDim[1]
-                    x1 = self.gridPos[0] + (distanceX * i)
-                    x2 = self.gridPos[0] + (distanceX * i) + self.gridSize[0]
-                    y1 = self.gridPos[1] + (distanceY * j)
-                    y2 = self.gridPos[1] + (distanceY * j) + self.gridSize[1]
-                    Line(points=[x1, y1, x1, y2], width=1)        
-                    Line(points=[x1, y1, x2, y1], width=1)
-
-
-
-
-
-
 class TouchBlocks(FloatLayout):
     def __init__(self):
         super(TouchBlocks, self).__init__()
@@ -110,68 +71,85 @@ class TouchBlocks(FloatLayout):
 class TouchBlocksApp(App):
     title = 'Touch Blocks'
 
-    def build(self):
+    def __init__(self):
+        super(TouchBlocksApp, self).__init__()
+
+        # Window init
+        Window.clearcolor = (1, 1, 1, 1)
         Window.bind(on_resize=self.on_resize)
 
-        Window.clearcolor = (1, 1, 1, 1)
+
+    def on_resize(self, window, width, height):
+        self.topGridsLayout.width = Window.width
+        self.topGridsLayout.height = Window.height/3*2
+        self.bottomGridLayout.width = Window.height/2
+        self.topGridSourceLayout.height = Window.height/2
+        self.topGridSourceLayout.width = self.topGridSourceLayout.height
+        self.topGridTargetLayout.height = Window.height/2
+        self.topGridTargetLayout.width = self.topGridTargetLayout.height        
+        return True
+
+    def build(self):
+
         # return TouchBlocks()
         # create a default grid layout with custom width/height
-        mainLayout = GridLayout(cols=1, padding=10, spacing=10)
+        self.mainLayout = GridLayout(cols=1)
 
-        topGridsLayout = GridLayout(cols=2, padding=10, spacing=10)
-        bottomGridLayout = GridLayout(cols=6, padding=10, spacing=10)
+        self.topGridsLayout = GridLayout(cols=2, size_hint=(None,None))
+        self.bottomGridLayout = GridLayout(cols=6, size_hint=(None,1))
 
-        topGridSourceLayout = GridLayout(cols=6, rows=6, padding=10, spacing=0, size_hint=(None,None))
-        topGridTargetLayout = GridLayout(cols=6, rows=6, padding=10, spacing=0, size_hint=(None,None))
+        self.topGridSourceLayout = GridLayout(cols=6, rows=6, size_hint=(None,None))
+        self.topGridTargetLayout = GridLayout(cols=6, rows=6, size_hint=(None,None))
 
+        self.topGridsLayout.width = Window.width
+        self.topGridsLayout.height = Window.height/3*2
+        self.bottomGridLayout.width = Window.height/2 * 1.5 # bottom buttons scaling factor
 
-        # topGridSourceLayout.width = topGridsLayout.height
+        self.topGridSourceLayout.height = Window.height/2
+        self.topGridTargetLayout.height = Window.height/2
+        self.topGridSourceLayout.width = self.topGridSourceLayout.height
+        self.topGridTargetLayout.width = self.topGridTargetLayout.height   
 
-        # topGridsLayout.bind(width=Window.setter('width'))
+        topGridSourceAnchorLayout = AnchorLayout(anchor_x='center', anchor_y='center')
+        topGridTargetAnchorLayout = AnchorLayout(anchor_x='center', anchor_y='center')
+        bottomGridAnchorLayout = AnchorLayout(anchor_x='center', anchor_y='center')
+        topGridSourceAnchorLayout.add_widget(self.topGridSourceLayout)
+        topGridTargetAnchorLayout.add_widget(self.topGridTargetLayout)
+        bottomGridAnchorLayout.add_widget(self.bottomGridLayout)
+        
+        self.topGridsLayout.add_widget(topGridSourceAnchorLayout)
+        self.topGridsLayout.add_widget(topGridTargetAnchorLayout)
 
-        # when we add children to the grid layout, its size doesn't change at
-        # all. we need to ensure that the height will be the minimum required
-        # to contain all the childs. (otherwise, we'll child outside the
-        # bounding box of the childs)
-        # mainLayout.bind(minimum_height=mainLayout.setter('height'))
-        # topGridsLayout.bind(minimum_height=topGridsLayout.setter('height'))
-        # bottomGridLayout.bind(minimum_height=bottomGridLayout.setter('height'))
-        # topGridSourceLayout.bind(minimum_height=topGridSourceLayout.setter('height'))
-        # topGridTargetLayout.bind(minimum_height=topGridTargetLayout.setter('height'))
+        self.mainLayout.add_widget(self.topGridsLayout)
+        self.mainLayout.add_widget(bottomGridAnchorLayout)
 
-        # add layouts to each other
-        mainLayout.add_widget(topGridsLayout)
-        mainLayout.add_widget(bottomGridLayout)
-
-        topGridsLayout.add_widget(topGridSourceLayout)
-        topGridsLayout.add_widget(topGridTargetLayout)
 
         # add buttons into topGridSourceLayout 
         for i in range(36):
             wimg = Image(source='assets/png/box_empty.png')
-            topGridSourceLayout.add_widget(wimg)
+            self.topGridSourceLayout.add_widget(wimg)
         # add buttons into topGridTargetLayout 
         for i in range(36):
             wimg = Image(source='assets/png/box_empty.png')
-            topGridTargetLayout.add_widget(wimg)
+            self.topGridTargetLayout.add_widget(wimg)
 
         # add buttons into bottomGridLayout 
         for i in range(6):
             wimg = Image(source='assets/png/box_empty.png')
-            bottomGridLayout.add_widget(wimg)
+            self.bottomGridLayout.add_widget(wimg)
+
+
+        pyramid = Image(source='assets/png/pyramid_orange.png', pos_hint=(1,1))
+        wimg.add_widget(pyramid)
 
         # create a scroll view, with a size < size of the grid
         # root = ScrollView(size_hint=(None, None), size=(500, 320),
         #         pos_hint={'center_x': .5, 'center_y': .5}, do_scroll_x=True)
         # root.add_widget(layout)
 
-        return mainLayout
+        return self.mainLayout
 
 
-    def on_resize(self, window, width, height):
-        #self.gridSize = [width, height]
-        print("Fff")
-        return True
 
     def on_pause(self):
         return True
